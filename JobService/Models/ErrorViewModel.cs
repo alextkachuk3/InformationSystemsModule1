fc000fc05001c821f20d9e6dfe027987baaf9000
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2022-10-03 00:50:40 +0300

    Initial commit

---
+++ JobService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
